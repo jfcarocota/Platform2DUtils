77e8752ff70313eada802fc97b7cfb7864903d87
jfcarocota <jfcarocota@gmail.com>
2020-03-11 07:30:18 -0700

    publish my package
    este es el primer commit oficial de nuestra paquetería

---
+++ Platform2DUtils/Memory Management/GameData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class GameData 
+{
+    [SerializeField]
+    Player player;
+    [SerializeField]
+    Vector3 playerPos;
+    [SerializeField]
+    string gameName;
+
+    public GameData(){}
+
+    public GameData(Vector3 playerPos, string gameName)
+    {
+        this.playerPos = playerPos;
+        this.gameName = gameName;
+    }
+
+    public Player Player
+    {
+        get => player;
+        set => player = value;
+    }
+    public Vector3 PlayerPos { get => playerPos; set => playerPos = value; }
+    public string GameName { get => gameName; set => gameName = value; }
+}
